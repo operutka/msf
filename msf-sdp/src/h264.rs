906423a8c4e9d0823a26bfd37c5be66f7d6688e5
Ondrej Perutka <perutka.ondrej@gmail.com>
2025-12-12 17:28:16 +0100

    Implement H.264 parameter parsing

--- msf-sdp/src/h264.rs
+++ msf-sdp/src/h264.rs
@@ -1,8 +1,11 @@
 //! H.264 extensions.
 
-use std::fmt::{self, Display, Formatter};
+use std::{
+    fmt::{self, Display, Formatter},
+    str::FromStr,
+};
 
-use base64::{display::Base64Display, prelude::BASE64_STANDARD};
+use base64::{display::Base64Display, engine::Engine, prelude::BASE64_STANDARD, DecodeError};
 use bytes::Bytes;
 use msf_util::h264::{extract_nal_unit, InvalidByteStream};
 
@@ -25,10 +28,18 @@ impl From<InvalidByteStream> for InvalidParameters {
     }
 }
 
+impl From<InvalidProfileLevelId> for InvalidParameters {
+    fn from(_: InvalidProfileLevelId) -> Self {
+        Self
+    }
+}
+
 /// H.264 format parameters.
 #[derive(Clone)]
 pub struct H264Parameters {
     packetization_mode: u8,
+    interleaving_depth: Option<u16>,
+    max_don_diff: Option<u16>,
     profile_level_id: Option<ProfileLevelId>,
     parameter_sets: Option<Bytes>,
 }
@@ -66,17 +77,71 @@ impl H264Parameters {
 
         let res = Self {
             packetization_mode,
+            interleaving_depth: None,
+            max_don_diff: None,
             profile_level_id,
             parameter_sets,
         };
 
         Ok(res)
     }
+
+    /// Get the packetization mode.
+    #[inline]
+    pub fn packetization_mode(&self) -> u8 {
+        self.packetization_mode
+    }
+
+    /// Get the interleaving depth.
+    #[inline]
+    pub fn interleaving_depth(&self) -> Option<u16> {
+        self.interleaving_depth
+    }
+
+    /// Set the interleaving depth.
+    #[inline]
+    pub fn with_interleaving_depth(mut self, depth: u16) -> Self {
+        self.interleaving_depth = Some(depth);
+        self
+    }
+
+    /// Get the maximum DON difference.
+    #[inline]
+    pub fn max_don_diff(&self) -> Option<u16> {
+        self.max_don_diff
+    }
+
+    /// Set the maximum DON difference.
+    #[inline]
+    pub fn with_max_don_diff(mut self, max_don_diff: u16) -> Self {
+        self.max_don_diff = Some(max_don_diff);
+        self
+    }
+
+    /// Get the profile-level ID.
+    #[inline]
+    pub fn profile_level_id(&self) -> Option<ProfileLevelId> {
+        self.profile_level_id
+    }
+
+    /// Get the parameter sets.
+    #[inline]
+    pub fn parameter_sets(&self) -> Option<&Bytes> {
+        self.parameter_sets.as_ref()
+    }
 }
 
 impl Display for H264Parameters {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "packetization-mode={}", self.packetization_mode)?;
+
+        if let Some(depth) = self.interleaving_depth {
+            write!(f, ";sprop-interleaving-depth={depth}")?;
+        }
+
+        if let Some(max_don_diff) = self.max_don_diff {
+            write!(f, ";sprop-max-don-diff={max_don_diff}")?;
+        }
 
         if let Some(profile_level_id) = self.profile_level_id.as_ref() {
             write!(f, ";profile-level-id={profile_level_id}")?;
@@ -98,9 +163,87 @@ impl Display for H264Parameters {
     }
 }
 
-/// Helper struct.
+impl FromStr for H264Parameters {
+    type Err = InvalidParameters;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let params = s
+            .split(';')
+            .map(str::trim)
+            .filter(|param| !param.is_empty());
+
+        let mut packetization_mode = None;
+        let mut interleaving_depth = None;
+        let mut max_don_diff = None;
+        let mut profile_level_id = None;
+        let mut parameter_sets = None;
+
+        for param in params {
+            let (key, value) = param.split_once('=').ok_or(InvalidParameters)?;
+
+            let key = key.trim();
+            let value = value.trim();
+
+            match key {
+                "packetization-mode" => packetization_mode = Some(value),
+                "sprop-interleaving-depth" => interleaving_depth = Some(value),
+                "sprop-max-don-diff" => max_don_diff = Some(value),
+                "profile-level-id" => profile_level_id = Some(value),
+                "sprop-parameter-sets" => parameter_sets = Some(value),
+                _ => (),
+            }
+        }
+
+        let packetization_mode = packetization_mode
+            .unwrap_or("0")
+            .parse::<u8>()
+            .map_err(|_| InvalidParameters)?;
+
+        let interleaving_depth = interleaving_depth
+            .map(u16::from_str)
+            .transpose()
+            .map_err(|_| InvalidParameters)?;
+
+        let max_don_diff = max_don_diff
+            .map(u16::from_str)
+            .transpose()
+            .map_err(|_| InvalidParameters)?;
+
+        let profile_level_id = profile_level_id.map(ProfileLevelId::from_str).transpose()?;
+
+        let parameter_sets = parameter_sets
+            .map(parse_parameter_sets)
+            .transpose()
+            .map_err(|_| InvalidParameters)?;
+
+        let res = Self {
+            packetization_mode,
+            interleaving_depth,
+            max_don_diff,
+            profile_level_id,
+            parameter_sets,
+        };
+
+        Ok(res)
+    }
+}
+
+/// Invalid H.264 profile-level ID.
+#[derive(Debug, Copy, Clone)]
+pub struct InvalidProfileLevelId;
+
+impl Display for InvalidProfileLevelId {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str("invalid h264 profile-level-id")
+    }
+}
+
+impl std::error::Error for InvalidProfileLevelId {}
+
+/// Profile-level ID.
 #[derive(Copy, Clone)]
-struct ProfileLevelId {
+pub struct ProfileLevelId {
     profile_idc: u8,
     constraints: u8,
     level_idc: u8,
@@ -108,12 +251,31 @@ struct ProfileLevelId {
 
 impl ProfileLevelId {
     /// Create a new profile level ID.
-    const fn new(profile_idc: u8, constraints: u8, level_idc: u8) -> Self {
+    #[inline]
+    pub const fn new(profile_idc: u8, constraints: u8, level_idc: u8) -> Self {
         Self {
             profile_idc,
             constraints,
             level_idc,
         }
+    }
+
+    /// Get the profile IDC.
+    #[inline]
+    pub const fn profile_idc(&self) -> u8 {
+        self.profile_idc
+    }
+
+    /// Get the constraints.
+    #[inline]
+    pub const fn constraints(&self) -> u8 {
+        self.constraints
+    }
+
+    /// Get the level IDC.
+    #[inline]
+    pub const fn level_idc(&self) -> u8 {
+        self.level_idc
     }
 }
 
@@ -125,4 +287,43 @@ impl Display for ProfileLevelId {
             self.profile_idc, self.constraints, self.level_idc
         )
     }
+}
+
+impl FromStr for ProfileLevelId {
+    type Err = InvalidProfileLevelId;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 6 {
+            return Err(InvalidProfileLevelId);
+        }
+
+        let n = u32::from_str_radix(s, 16).map_err(|_| InvalidProfileLevelId)?;
+
+        let profile_idc = (n >> 16) as u8;
+        let constraints = (n >> 8) as u8;
+        let level_idc = n as u8;
+
+        let res = Self::new(profile_idc, constraints, level_idc);
+
+        Ok(res)
+    }
+}
+
+/// Parse parameter sets from a given Base64 list.
+fn parse_parameter_sets(s: &str) -> Result<Bytes, DecodeError> {
+    let mut res = Vec::new();
+
+    for b64 in s.split(',') {
+        let input = b64.trim();
+
+        if input.is_empty() {
+            continue;
+        }
+
+        res.extend_from_slice(&[0, 0, 1]);
+
+        Engine::decode_vec(&BASE64_STANDARD, input, &mut res)?;
+    }
+
+    Ok(res.into())
 }
